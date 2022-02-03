e51937648373499cb7c534cd8cba9d8b811323f2
Florian Bussmann <florian.bussmann@fh-muenster.de>
2022-02-03 15:13:15 +0100

    Add input for dependencies

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,16 @@
-#!/bin/sh -l
+#!/bin/bash
 
 echo "Thanks for using this GH action!"
 input_file=$1
 output_format=$2
+dependencies=$3
 
 ln -s /render.R .
+
+if ! [ -z $dependencies ]
+then
+    packages=\'${dependencies//,/\',\'}\'
+    Rscript -e "install.packages(c($packages))"
+fi
 
 Rscript render.R $input_file $output_format
